ada6bd75b65a924a35e87027ffc88ba21cc57292
Noalino <bgelineau.dev@gmail.com>
2024-03-09 14:45:36 +0100

    Create Card model

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Noalino on 09/03/2024.
+//
+
+import Foundation
+
+struct Card {
+    var prompt: String
+    var answer: String
+
+    static let example = Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+}
