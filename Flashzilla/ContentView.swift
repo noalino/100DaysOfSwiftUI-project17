87290c33b773866e58c663fd5b3422bd74ac5020
Noalino <bgelineau.dev@gmail.com>
2024-03-11 10:03:02 +0100

    Fix draggable nested cards

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -47,6 +47,7 @@ struct ContentView: View {
                             }
                         }
                         .stacked(at: index, in: cards.count)
+                        .allowsHitTesting(index == cards.count - 1)
                     }
                 }
                 .allowsHitTesting(timeRemaining > 0)
